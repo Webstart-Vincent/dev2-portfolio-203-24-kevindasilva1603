4b1cefd702b3329f06967b6fb701c0b9715eb86d
kevindasilva1603 <kevinsilva1603@gmail.com>
2024-01-24 14:14:56 +0100

    delete

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "/Users/kevindasilva/Documents/portfolionext/components/provider";
+import Provider from "@/components/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
